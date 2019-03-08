af2ca00b1f8e3c764bcd0d17239e5a07cc18080b
daisuke <dada.00131@google.com>
2019-03-08 15:48:30 +0900

    ユーザーshowの投稿一覧といいね一覧の型修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
 
     def create
         @post = Post.find(params[:post_id])
-        @comment = Comment.new(body: comment_params[:body], user_id: @post.user.id, post_id: @post.id)
+        @comment = Comment.new(body: comment_params[:body], user_id: current_user.id, post_id: @post.id)
         @comment.save
         redirect_to post_path(@post)     
     end
